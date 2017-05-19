c11522b3388289d10a9c7bede317b31b57eec648
Lionel HERVIER <lhervier@asi.fr>
2017-05-19 10:50:24 +0200

    Rest Sample: Le buils Maven ne se faisait pas jusqu'au bout

--- rest-sample/src/main/java/fr/asso/afer/rest/sample/Application.java
+++ rest-sample/src/main/java/fr/asso/afer/rest/sample/Application.java
@@ -1,12 +1,8 @@
 package fr.asso.afer.rest.sample;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Classe initialisant SpringBoot.
@@ -15,25 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 	
-	/**
-	 * Les origines autorisées
-	 */
-	@Value("${cors.allowedOrigins}")
-	private String[] allowedOrigins;
-	
-	/**
-	 * Pour envoyer les en têtes CORS
-	 * @return
-	 */
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins(Application.this.allowedOrigins)
-						.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE");
-			}
-		};
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 }
--- rest-sample/src/main/java/fr/asso/afer/rest/sample/SpringBootConfig.java
+++ rest-sample/src/main/java/fr/asso/afer/rest/sample/SpringBootConfig.java
@@ -1,11 +1,38 @@
 package fr.asso.afer.rest.sample;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 @ComponentScan(basePackages = {"fr.asso.afer.rest"})
 public class SpringBootConfig {
+	
+	/**
+	 * Les origines autorisées
+	 */
+	@Value("${cors.allowedOrigins}")
+	private String[] allowedOrigins;
+	
+	/**
+	 * Pour envoyer les en têtes CORS
+	 * @return
+	 */
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(SpringBootConfig.this.allowedOrigins)
+						.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }

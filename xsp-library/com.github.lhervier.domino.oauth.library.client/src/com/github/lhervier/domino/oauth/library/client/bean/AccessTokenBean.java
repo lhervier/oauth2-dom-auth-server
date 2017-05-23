85d43aebafaf131d3c872248c6823a702a354f73
Lionel HERVIER <lhervier@asi.fr>
2017-05-23 12:05:52 +0200

    Front Sample: Le API accessToken ne retourne pas de 403

--- xsp-library/com.github.lhervier.domino.oauth.library.client/src/com/github/lhervier/domino/oauth/library/client/bean/AccessTokenBean.java
+++ xsp-library/com.github.lhervier.domino.oauth.library.client/src/com/github/lhervier/domino/oauth/library/client/bean/AccessTokenBean.java
@@ -17,17 +17,8 @@ public class AccessTokenBean {
 	 * @throws IOException
 	 */
 	public void sendToken() throws IOException {
-		String accessToken = (String) JSFUtils.getSessionScope().get("access_token");
-		
-		// On ne l'a pas => On retourne une 403
-		if( accessToken == null ) {
-			JSFUtils.send403();
-			
-		// On l'a => On le retourne
-		} else {
-			Map<String, Object> token = new HashMap<String, Object>();
-			token.put("access_token", JSFUtils.getSessionScope().get("access_token"));
-			JSFUtils.sendJson(token);
-		}
+		Map<String, Object> token = new HashMap<String, Object>();
+		token.put("access_token", JSFUtils.getSessionScope().get("access_token"));
+		JSFUtils.sendJson(token);
 	}
 }

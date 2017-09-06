9df836980b4130b8cf9c4ba29dcd4397ee52cb32
Lionel HERVIER <lhervier@asi.fr>
2017-09-06 16:53:49 +0200

    [Front-Sample] Refactor de l'exemple angular

--- front-sample-ondisk/WebContent/app/app.js
+++ front-sample-ondisk/WebContent/app/app.js
@@ -1,19 +1,23 @@
 var sampleApp = angular.module('sampleApp', ['ngResource']);
 
-sampleApp.controller('SampleController', ['$rootScope', '$resource', 'tokenService', 'alerteService', function($rootScope, $resource, tokenService, alerteService) {
+sampleApp.controller('SampleController', ['$rootScope', '$resource', '$window', 'tokenService', function($rootScope, $resource, $window, tokenService) {
 	
 	var ths = this;
+	this._reconnectUrl = "init.xsp?redirect_url=" + encodeURIComponent($window.location);
 	
 	this.alerte = null;
 	this.reconnectUrl = null;
 	this.userInfo = null;
-	this.accessToken = null;
+	this.accessToken = null;		// Juste pour info...
 	
 	this.loadUserInfo = function() {
 		ths.userInfo = $resource(ths.param.restServer + '/userInfo').get(
 				function() {},
-				function(error) {
-					alerteService.error("Erreur à la récupération des infos utilisateur...");
+				function(reason) {
+					if( "oauth2.needs_reconnect" == reason )
+						ths.reconnectUrl = ths._reconnectUrl;
+					else
+						ths.alerte = "Erreur à la récupération des infos utilisateur : " + reason;
 				});
 	};
 	
@@ -25,81 +29,56 @@ sampleApp.controller('SampleController', ['$rootScope', '$resource', 'tokenServi
 		}
 	});
 	
-	$rootScope.$on("reconnect", function(event, url) {
-		ths.reconnectUrl = url;
+	$rootScope.$on("oauth2.reconnect", function(event, url) {
+		ths.reconnectUrl = ths._reconnectUrl;
 	});
 	
-	// Charge le paramétrage, puis le access_token au démarrage de la page
-	// On force la reconnexion
-	this.param = $resource('param.xsp').get();
-	this.param.$promise.then(
-			function() {
-				tokenService.getToken(true).then(
-						function(token) {
-							ths.accessToken = token.access_token;
-						},
-						function() {
-							alerteService.error("Erreur à la récupération du access_token");
-						}
-				);
+	// Charge le paramétrage
+	$resource('param.xsp').get(function(param) {
+		ths.param = param;;
+	});
+	
+	// Charge le token. On force la reconnexion s'il n'est pas en session.
+	tokenService.getToken().then(
+			function(token) {
+				ths.accessToken = token.access_token;
 			},
 			function() {
-				alerteService.error("Erreur à la récupération des paramètres locaux");
+				$window.location = "init.xsp?redirect_url=" + encodeURIComponent($window.location);
 			}
 	);
 }]);
 
-sampleApp.factory('reconnectService', ['$rootScope', '$window', function($rootScope, $window) {
-	return {
-		reconnect: function(force) {
-			var connectUrl = "init.xsp?redirect_url=" + encodeURIComponent($window.location);
-			if( force )
-				$window.location = connectUrl;
-			else
-				$rootScope.$emit("reconnect", connectUrl);
-		}
-	};
-}]);
-
-sampleApp.factory('alerteService', ['$rootScope', function($rootScope) {
-	return {
-		info: function(message) {
-			$rootScope.$emit("alerte", "info", message);
-		},
-		warning: function(message, url) {
-			$rootScope.$emit("alerte", "warning", message);
-		},
-		error: function(message, url) {
-			$rootScope.$emit("alerte", "error", message);
-		}
-	}
-}]);
-
-sampleApp.factory('tokenService', ['$q', '$resource', '$window', 'alerteService', 'reconnectService', function($q, $resource, $window, alerteService, reconnectService) {
+sampleApp.factory('tokenService', ['$rootScope', '$q', '$resource', '$window', function($rootScope, $q, $resource, $window) {
 	var svc = {
 		token: null,		// The token
 		iss: null,			// Token issued date
-		_getToken: function(force, url) {
-			var result = $resource(url).get();
-			return result.$promise.then(
-				function() {
-					var deferred = $q.defer();
+		_getToken: function(url) {
+			var ths = this;
+			
+			var deferred = $q.defer();
+			$resource(url).get().$promise.then(deferred.resolve, deferred.reject);
+			
+			return deferred.promise.then(
+				function(result) {
+					var def = $q.defer();
 					if( !result.access_token ) {	// On n'a pas le token, même si le serveur a repondu avec un code 200
-						reconnectService.reconnect(force);
-						deferred.reject();
+						def.reject();
 					} else {
 						svc.token = result.access_token;
 						svc.iss = new Date().getTime();
-						deferred.resolve(result);
+						def.resolve(result);
 					}
-					return deferred.promise;
+					return def.promise;
 				},
 				function() {						// Le serveur a répondu autre chose que 200
-					reconnectService.reconnect(force);
+					var def = $q.defer();
+					def.reject();
+					return def.promise;
 				}
 			);
 		},
-		getToken: function(force) {
+		getToken: function() {
 			// On a le token => On le retourne
 			if( svc.token ) {
 				var defer = $q.defer();
@@ -108,7 +87,7 @@ sampleApp.factory('tokenService', ['$q', '$resource', '$window', 'alerteService'
 			}
 			
 			// On ne l'a pas, on va le chercher
-			return this._getToken(force, 'accessToken.xsp');
+			return this._getToken('accessToken.xsp');
 		},
 		refreshToken: function() {
 			// On ne rafraîchit que si le token n'a pas été mise à jour il y a peu de temps (10s)
@@ -117,7 +96,7 @@ sampleApp.factory('tokenService', ['$q', '$resource', '$window', 'alerteService'
 				deferred.reject();
 				return deferred.promise;
 			} else
-				return this._getToken(false, 'refresh.xsp');
+				return this._getToken('refresh.xsp');
 		}
 	};
 	return svc;
@@ -143,7 +122,7 @@ sampleApp.config(['$httpProvider', function($httpProvider) {
 					return config;
 				
 				var tokenService = $injector.get('tokenService');
-				return tokenService.getToken(false).then(
+				return tokenService.getToken().then(
 						function(token) {
 							config.headers.authorization = "Bearer " + token.access_token;
 							return config;
@@ -158,20 +137,24 @@ sampleApp.config(['$httpProvider', function($httpProvider) {
 				);
 			},
 			
-			responseError: function(rejection) {
-				if( rejection.status != 403 || !shouldProcess(rejection.config.url) )
-					return $q.reject(rejection);;
-				
-				var tokenService = $injector.get('tokenService');
-				var $http = $injector.get('$http');
-				return tokenService.refreshToken().then(
-						function(token) {
-							return $http(rejection.config);
-						},
-						function(error) {
-							console.log('XXX');
-						}
-				);
+			responseError: function(response) {
+				if( response.status == 403 && shouldProcess(response.config.url) ) {
+					var tokenService = $injector.get('tokenService');
+					var $http = $injector.get('$http');
+					var deferred = $q.defer();
+					tokenService.refreshToken().then(deferred.resolve, deferred.reject);
+					return deferred.promise.then(
+							function(token) {
+								return $http(response.config);
+							},
+							function(error) {
+								var deferred = $q.defer();
+								deferred.reject("oauth2.needs_reconnect");
+								return deferred.promise;
+							}
+					);
+				}
+				return $q.reject(response);
 			}
 		};
 	}]);

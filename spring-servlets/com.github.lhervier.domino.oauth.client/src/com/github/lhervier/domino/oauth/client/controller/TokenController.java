b70a03f089d48b19970895c986eea5270967fb49
Lionel HERVIER <lhervier@asi.fr>
2017-09-29 16:55:33 +0200

    [Client] Splits token controller in two parts

--- spring-servlets/com.github.lhervier.domino.oauth.client/src/com/github/lhervier/domino/oauth/client/controller/TokenController.java
+++ spring-servlets/com.github.lhervier.domino.oauth.client/src/com/github/lhervier/domino/oauth/client/controller/TokenController.java
@@ -1,14 +1,7 @@
 package com.github.lhervier.domino.oauth.client.controller;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.text.ParseException;
-
 import javax.servlet.http.HttpSession;
 
-import lotus.domino.NotesException;
-
-import org.codehaus.jackson.annotate.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -17,14 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.lhervier.domino.oauth.client.Constants;
-import com.github.lhervier.domino.oauth.client.ex.OauthClientException;
-import com.github.lhervier.domino.oauth.client.ex.RefreshTokenException;
-import com.github.lhervier.domino.oauth.client.model.GrantResponse;
-import com.github.lhervier.domino.oauth.client.utils.Utils;
-import com.github.lhervier.domino.oauth.common.model.error.GrantError;
-import com.github.lhervier.domino.oauth.common.utils.Callback;
-import com.github.lhervier.domino.oauth.common.utils.ValueHolder;
-import com.github.lhervier.domino.spring.servlet.NotesContext;
+import com.github.lhervier.domino.oauth.client.model.TokenResponse;
 
 @Controller
 public class TokenController {
@@ -36,34 +22,10 @@ public class TokenController {
 	private HttpSession httpSession;
 	
 	/**
-	 * The notes context
-	 */
-	@Autowired
-	private NotesContext notesContext;
-	
-	/**
 	 * The spring environment
 	 */
 	@Autowired
 	private Environment env;
-	
-	/**
-	 * The response class
-	 */
-	public static class TokenResponse {
-		@JsonProperty("access_token")
-		private String accessToken;
-		@JsonProperty("id_token")
-		private String idToken;
-		@JsonProperty("user_info_endpoint")
-		private String userInfoEndpoint;
-		public String getAccessToken() {return accessToken;}
-		public void setAccessToken(String accessToken) {this.accessToken = accessToken;}
-		public String getIdToken() {return idToken;}
-		public void setIdToken(String idToken) {this.idToken = idToken;}
-		public String getUserInfoEndpoint() { return userInfoEndpoint; }
-		public void setUserInfoEndpoint(String userInfoEndpoint) { this.userInfoEndpoint = userInfoEndpoint; }
-	}
 	
 	/**
 	 * Send the access token
@@ -75,66 +37,5 @@ public class TokenController {
 		resp.setIdToken((String) this.httpSession.getAttribute(Constants.SESSION_ID_TOKEN));
 		resp.setUserInfoEndpoint(this.env.getProperty("oauth2.client.endpoints.userInfo"));
 		return resp;
-	}
-	
-	/**
-	 * Refresh the token
-	 * @throws IOException 
-	 * @throws NotesException 
-	 * @throws UnsupportedEncodingException 
-	 */
-	@RequestMapping(value = "/refresh", method = RequestMethod.GET)
-	public @ResponseBody TokenResponse refreshToken() throws OauthClientException, RefreshTokenException {
-		try {
-			String refreshToken = (String) this.httpSession.getAttribute(Constants.SESSION_REFRESH_TOKEN);
-			
-			// No refresh token => Unable to ask for a new one
-			if( refreshToken == null )
-				return this.accessToken();
-			
-			final ValueHolder<RefreshTokenException> ex = new ValueHolder<RefreshTokenException>();
-			Utils.createConnection(
-					this.notesContext.getServerSession(), 
-					Boolean.parseBoolean(this.env.getProperty("oauth2.client.disableHostVerifier")), 
-					this.env.getProperty("oauth2.client.secret"),
-					this.env.getProperty("oauth2.client.endpoints.token")
-			)
-			.setTextContent(
-					new StringBuffer()
-							.append("grant_type=refresh_token&")
-							.append("refresh_token=").append(refreshToken)
-							.toString(), 
-					"UTF-8"
-			)
-			
-			// OK => Met à jour la session et retourne le token
-			.onOk(new Callback<GrantResponse>() {
-				@Override
-				public void run(GrantResponse grant) throws IOException, ParseException {
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_REFRESH_TOKEN, grant.getRefreshToken());
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_ACCESS_TOKEN, grant.getAccessToken());
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_ID_TOKEN, grant.getIdToken());
-				}
-			})
-			
-			// Erreur => Non autorisé
-			.onError(new Callback<GrantError>() {
-				@Override
-				public void run(GrantError error) throws IOException {
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_REFRESH_TOKEN, null);
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_ACCESS_TOKEN, null);
-					TokenController.this.httpSession.setAttribute(Constants.SESSION_ID_TOKEN, null);
-					ex.set(new RefreshTokenException(error));
-				}
-			})
-			.execute();
-			
-			if( ex.get() != null )
-				throw ex.get();
-			
-			return this.accessToken();
-		} catch(IOException e) {
-			throw new OauthClientException(e);
-		}
 	}
 }
---
+++ spring-servlets/com.github.lhervier.domino.oauth.client/src/com/github/lhervier/domino/oauth/client/model/TokenResponse.java
@@ -0,0 +1,18 @@
+package com.github.lhervier.domino.oauth.client.model;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class TokenResponse {
+	@JsonProperty("access_token")
+	private String accessToken;
+	@JsonProperty("id_token")
+	private String idToken;
+	@JsonProperty("user_info_endpoint")
+	private String userInfoEndpoint;
+	public String getAccessToken() {return accessToken;}
+	public void setAccessToken(String accessToken) {this.accessToken = accessToken;}
+	public String getIdToken() {return idToken;}
+	public void setIdToken(String idToken) {this.idToken = idToken;}
+	public String getUserInfoEndpoint() { return userInfoEndpoint; }
+	public void setUserInfoEndpoint(String userInfoEndpoint) { this.userInfoEndpoint = userInfoEndpoint; }
+}
